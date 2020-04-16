2f804f78797dea7a86d2c6198463310c5cd76aaa
fulltech777 <webstar2222@gmail.com>
2020-04-17 00:43:25 +0800

    Recreate geolocator component with parameters

--- src/lib/components/Geolocator/GeolocatorComponent.react.js
+++ src/lib/components/Geolocator/GeolocatorComponent.react.js
@@ -1,62 +1,61 @@
-import React, {Component} from 'react';
-import { geolocated } from "react-geolocated";
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Geolocator from './Geolocator';
 
 class GeolocatorComponent extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {value: props.value};
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        const {
-            isGeolocationAvailable,
-            isGeolocationEnabled,
-            coords,
-        } = this.props;
-
-        return !this.props.isGeolocationAvailable ? (
-            <div>Your browser does not support Geolocation</div>
-        ) : !this.props.isGeolocationEnabled ? (
-            <div>Geolocation is not enabled</div>
-        ) : this.props.coords ? (
-            <table>
-                <tbody>
-                    <tr>
-                        <td>latitude</td>
-                        <td>{this.props.coords.latitude}</td>
-                    </tr>
-                    <tr>
-                        <td>longitude</td>
-                        <td>{this.props.coords.longitude}</td>
-                    </tr>
-                    <tr>
-                        <td>altitude</td>
-                        <td>{this.props.coords.altitude}</td>
-                    </tr>
-                    <tr>
-                        <td>heading</td>
-                        <td>{this.props.coords.heading}</td>
-                    </tr>
-                    <tr>
-                        <td>speed</td>
-                        <td>{this.props.coords.speed}</td>
-                    </tr>
-                </tbody>
-            </table>
-        ) : (
-            <div>Getting the location data&hellip; </div>
-        );
-    }
+  render() {
+    return (
+      <div id={this.props.id}>
+        <Geolocator 
+          positionOptions = {this.props.positionOptions}
+          watchPosition = {this.props.watchPosition}
+          userDecisionTimeout = {this.props.userDecisionTimeout}
+          suppressLocationOnMount = {this.props.suppressLocationOnMount}          
+          isOptimisticGeolocationEnabled = {this.props.isOptimisticGeolocationEnabled}
+          geolocationProvider = {navigator.geolocation}          
+          onSuccess = {this.props.onSuccess}
+          onError = {this.props.onError}
+          errorMsg = {this.props.errorMsg}
+        />
+      </div>
+    )
+  }
 }
 
-GeolocatorComponent.defaultProps={
-    id: null
+GeolocatorComponent.defaultProps = {
+  id: null,
+  positionOptions: {
+    enableHighAccuracy: true,
+    maximumAge: 0,
+    timeout: 5000,
+  },
+  watchPosition: true,
+  userDecisionTimeout: null,
+  suppressLocationOnMount: false,
+  isOptimisticGeolocationEnabled: true,
+  errorMsg: {
+    isGeolocationAvailable: "Your browser does not support Geolocation",
+    isGeolocationEnabled: "Geolocation is not enabled"
+  },
+  onSuccess: function(success){ console.log(success) },
+  onError: function(error){ console.log(error)}
 };
 
 GeolocatorComponent.propTypes = {
-    id: PropTypes.string
+  id: PropTypes.string,
+  positionOptions: PropTypes.object,
+  watchPosition: PropTypes.bool,
+  userDecisionTimeout: PropTypes.number,
+  suppressLocationOnMount: PropTypes.bool,
+  isOptimisticGeolocationEnabled: PropTypes.bool,  
+  errorMsg: PropTypes.object,
+  onSuccess: PropTypes.func,
+  onError: PropTypes.func,
 };
 
-export default geolocated()(GeolocatorComponent);
+export default GeolocatorComponent;
