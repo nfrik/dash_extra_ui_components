7c701d3a316ed5d7159903b11651a5b87f623b3b
nfrik <friknikolay@gmail.com>
2020-04-19 17:14:11 +0000

    updated usage.py

--- usage.py
+++ usage.py
@@ -1,13 +1,11 @@
+import json
+
 import dash
 import dash_html_components as html
-from dash.dependencies import Input, Output
-
 import extra_dash_ui_components as ex
-from sample_data.org_data import sample1, sample2
-from sample_data.list_component import cities, cars
-from sample_data.carousel import cars_carousel
+from dash.dependencies import Input, Output, State
 
-import json
+from sample_data.carousel import cars_carousel
 
 app = dash.Dash(__name__)
 data = [
@@ -15,58 +13,63 @@ data = [
         "key": "0",
         "label": "Documents",
         "data": "Documents Folder",
-                "icon": "pi pi-fw pi-inbox",
-                "children": [{
-                    "key": "0-0",
-                    "label": "Work",
-                    "data": "Work Folder",
-                    "icon": "pi pi-fw pi-cog",
-                    "children": [
-                        {"key": "0-0-0", "label": "Expenses.doc",
-                            "icon": "pi pi-fw pi-file", "data": "Expenses Document"},
-                        {"key": "0-0-1", "label": "Resume.doc",
-                            "icon": "pi pi-fw pi-file", "data": "Resume Document"}
-                    ]
-                },
-                    {
-                    "key": "0-1",
-                    "label": "Home",
-                    "data": "Home Folder",
-                    "icon": "pi pi-fw pi-home",
-                    "children": [{"key": "0-1-0", "label": "Invoices.txt", "icon": "pi pi-fw pi-file", "data": "Invoices for this month"}]
-                }]
+        "icon": "pi pi-fw pi-inbox",
+        "children": [{
+            "key": "0-0",
+            "label": "Work",
+            "data": "Work Folder",
+            "icon": "pi pi-fw pi-cog",
+            "children": [
+                {"key": "0-0-0", "label": "Expenses.doc",
+                 "icon": "pi pi-fw pi-file", "data": "Expenses Document"},
+                {"key": "0-0-1", "label": "Resume.doc",
+                 "icon": "pi pi-fw pi-file", "data": "Resume Document"}
+            ]
+        },
+            {
+                "key": "0-1",
+                "label": "Home",
+                "data": "Home Folder",
+                "icon": "pi pi-fw pi-home",
+                "children": [{"key": "0-1-0", "label": "Invoices.txt", "icon": "pi pi-fw pi-file",
+                              "data": "Invoices for this month"}]
+            }]
     },
     {
         "key": "1",
         "label": "Events",
         "data": "Events Folder",
-                "icon": "pi pi-fw pi-calendar",
-                "children": [
-                    {"key": "1-0", "label": "Meeting",
-                        "icon": "pi pi-fw pi-calendar-plus", "data": "Meeting"},
-                    {"key": "1-1", "label": "Product Launch",
-                        "icon": "pi pi-fw pi-calendar-plus", "data": "Product Launch"},
-                    {"key": "1-2", "label": "Report Review", "icon": "pi pi-fw pi-calendar-plus", "data": "Report Review"}]
+        "icon": "pi pi-fw pi-calendar",
+        "children": [
+            {"key": "1-0", "label": "Meeting",
+             "icon": "pi pi-fw pi-calendar-plus", "data": "Meeting"},
+            {"key": "1-1", "label": "Product Launch",
+             "icon": "pi pi-fw pi-calendar-plus", "data": "Product Launch"},
+            {"key": "1-2", "label": "Report Review", "icon": "pi pi-fw pi-calendar-plus", "data": "Report Review"}]
     },
     {
         "key": "2",
         "label": "Movies",
         "data": "Movies Folder",
+        "icon": "pi pi-fw pi-star",
+        "children": [{
+            "key": "2-0",
+            "icon": "pi pi-fw pi-star",
+            "label": "Al Pacino",
+            "data": "Pacino Movies",
+            "children": [{"key": "2-0-0", "label": "Scarface", "icon": "pi pi-fw pi-video", "data": "Scarface Movie"},
+                         {"key": "2-0-1", "label": "Serpico", "icon": "pi pi-fw pi-video", "data": "Serpico Movie"}]
+        },
+            {
+                "key": "2-1",
+                "label": "Robert De Niro",
                 "icon": "pi pi-fw pi-star",
-                "children": [{
-                    "key": "2-0",
-                    "icon": "pi pi-fw pi-star",
-                    "label": "Al Pacino",
-                    "data": "Pacino Movies",
-                    "children": [{"key": "2-0-0", "label": "Scarface", "icon": "pi pi-fw pi-video", "data": "Scarface Movie"}, {"key": "2-0-1", "label": "Serpico", "icon": "pi pi-fw pi-video", "data": "Serpico Movie"}]
-                },
-                    {
-                    "key": "2-1",
-                    "label": "Robert De Niro",
-                    "icon": "pi pi-fw pi-star",
-                    "data": "De Niro Movies",
-                    "children": [{"key": "2-1-0", "label": "Goodfellas", "icon": "pi pi-fw pi-video", "data": "Goodfellas Movie"}, {"key": "2-1-1", "label": "Untouchables", "icon": "pi pi-fw pi-video", "data": "Untouchables Movie"}]
-                }]
+                "data": "De Niro Movies",
+                "children": [
+                    {"key": "2-1-0", "label": "Goodfellas", "icon": "pi pi-fw pi-video", "data": "Goodfellas Movie"},
+                    {"key": "2-1-1", "label": "Untouchables", "icon": "pi pi-fw pi-video",
+                     "data": "Untouchables Movie"}]
+            }]
     }
 ]
 
@@ -89,7 +92,6 @@ first2 = 0
 rows2 = 10
 carData = []
 
-
 responsiveOptions = [
     {
         'breakpoint': '1024px',
@@ -108,64 +110,68 @@ responsiveOptions = [
     },
 ]
 
-
 data = [
     {
         "key": "0",
         "label": "Documents",
         "data": "Documents Folder",
-                "icon": "pi pi-fw pi-inbox",
-                "children": [{
-                    "key": "0-0",
-                    "label": "Work",
-                    "data": "Work Folder",
-                    "icon": "pi pi-fw pi-cog",
-                    "children": [
-                        {"key": "0-0-0", "label": "Expenses.doc",
-                            "icon": "pi pi-fw pi-file", "data": "Expenses Document"},
-                        {"key": "0-0-1", "label": "Resume.doc",
-                            "icon": "pi pi-fw pi-file", "data": "Resume Document"}
-                    ]
-                },
-                    {
-                    "key": "0-1",
-                    "label": "Home",
-                    "data": "Home Folder",
-                    "icon": "pi pi-fw pi-home",
-                    "children": [{"key": "0-1-0", "label": "Invoices.txt", "icon": "pi pi-fw pi-file", "data": "Invoices for this month"}]
-                }]
+        "icon": "pi pi-fw pi-inbox",
+        "children": [{
+            "key": "0-0",
+            "label": "Work",
+            "data": "Work Folder",
+            "icon": "pi pi-fw pi-cog",
+            "children": [
+                {"key": "0-0-0", "label": "Expenses.doc",
+                 "icon": "pi pi-fw pi-file", "data": "Expenses Document"},
+                {"key": "0-0-1", "label": "Resume.doc",
+                 "icon": "pi pi-fw pi-file", "data": "Resume Document"}
+            ]
+        },
+            {
+                "key": "0-1",
+                "label": "Home",
+                "data": "Home Folder",
+                "icon": "pi pi-fw pi-home",
+                "children": [{"key": "0-1-0", "label": "Invoices.txt", "icon": "pi pi-fw pi-file",
+                              "data": "Invoices for this month"}]
+            }]
     },
     {
         "key": "1",
         "label": "Events",
         "data": "Events Folder",
-                "icon": "pi pi-fw pi-calendar",
-                "children": [
-                    {"key": "1-0", "label": "Meeting",
-                        "icon": "pi pi-fw pi-calendar-plus", "data": "Meeting"},
-                    {"key": "1-1", "label": "Product Launch",
-                        "icon": "pi pi-fw pi-calendar-plus", "data": "Product Launch"},
-                    {"key": "1-2", "label": "Report Review", "icon": "pi pi-fw pi-calendar-plus", "data": "Report Review"}]
+        "icon": "pi pi-fw pi-calendar",
+        "children": [
+            {"key": "1-0", "label": "Meeting",
+             "icon": "pi pi-fw pi-calendar-plus", "data": "Meeting"},
+            {"key": "1-1", "label": "Product Launch",
+             "icon": "pi pi-fw pi-calendar-plus", "data": "Product Launch"},
+            {"key": "1-2", "label": "Report Review", "icon": "pi pi-fw pi-calendar-plus", "data": "Report Review"}]
     },
     {
         "key": "2",
         "label": "Movies",
         "data": "Movies Folder",
+        "icon": "pi pi-fw pi-star",
+        "children": [{
+            "key": "2-0",
+            "icon": "pi pi-fw pi-star",
+            "label": "Al Pacino",
+            "data": "Pacino Movies",
+            "children": [{"key": "2-0-0", "label": "Scarface", "icon": "pi pi-fw pi-video", "data": "Scarface Movie"},
+                         {"key": "2-0-1", "label": "Serpico", "icon": "pi pi-fw pi-video", "data": "Serpico Movie"}]
+        },
+            {
+                "key": "2-1",
+                "label": "Robert De Niro",
                 "icon": "pi pi-fw pi-star",
-                "children": [{
-                    "key": "2-0",
-                    "icon": "pi pi-fw pi-star",
-                    "label": "Al Pacino",
-                    "data": "Pacino Movies",
-                    "children": [{"key": "2-0-0", "label": "Scarface", "icon": "pi pi-fw pi-video", "data": "Scarface Movie"}, {"key": "2-0-1", "label": "Serpico", "icon": "pi pi-fw pi-video", "data": "Serpico Movie"}]
-                },
-                    {
-                    "key": "2-1",
-                    "label": "Robert De Niro",
-                    "icon": "pi pi-fw pi-star",
-                    "data": "De Niro Movies",
-                    "children": [{"key": "2-1-0", "label": "Goodfellas", "icon": "pi pi-fw pi-video", "data": "Goodfellas Movie"}, {"key": "2-1-1", "label": "Untouchables", "icon": "pi pi-fw pi-video", "data": "Untouchables Movie"}]
-                }]
+                "data": "De Niro Movies",
+                "children": [
+                    {"key": "2-1-0", "label": "Goodfellas", "icon": "pi pi-fw pi-video", "data": "Goodfellas Movie"},
+                    {"key": "2-1-1", "label": "Untouchables", "icon": "pi pi-fw pi-video",
+                     "data": "Untouchables Movie"}]
+            }]
     }
 ]
 
@@ -209,8 +215,23 @@ app.layout = html.Div(children=[
     ),
     html.H4('geolocator Preview'),
     ex.GeolocatorComponent(
-        id='geolocator'
+        id='geolocator',
     ),
+
+    html.Button('Geoloc Trigger',id='geoloc-trig'),
+    html.Div(id='geoloc-out'),
+
+    # ex.TrelloComponent(
+    #     draggable=True,
+    #     laneDraggable=True,
+    #     cardDraggable=True,
+    #     collapsibleLanes=True,
+    #     editable=True,
+    #     canAddLanes=True,
+    #     hideCardDeleteIcon=True,
+    #     editLaneTitle=True
+    # ),
+    html.Div(id='geolocator-output'),
 
     html.H4('breadcrumbs Preview'),
     ex.BreadcrumbsDashUiComponents(
@@ -270,16 +291,6 @@ app.layout = html.Div(children=[
     #     # className='', # css style
 
     # ),
-    ex.TrelloComponent(
-        draggable=True,
-        laneDraggable=True,
-        cardDraggable=True,
-        collapsibleLanes=True,
-        editable=True,
-        canAddLanes=True,
-        hideCardDeleteIcon=True,
-        editLaneTitle=True   
-    ),
     ex.CarouselComponent(
         id='input5',
         value=cars_carousel,
@@ -295,9 +306,6 @@ app.layout = html.Div(children=[
     html.Div(id='output0'),
     html.Div(id='output2'),
     html.Div(id='output5'),
-
-
-
 
     html.H4('Paginator Default Preview'),
     ex.PaginatorDashUiComponents(
@@ -316,9 +324,8 @@ app.layout = html.Div(children=[
         rowsPerPageOptions=[10, 20, 30],
         template="FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
     ),
-    
     html.Div(id='output1'),
-    html.H4('Datascroller inline Preview  -------------------------------'),
+    html.H4('Datascroller inline Preview'),
     html.Div([
         ex.DataScrollerDashUiComponents(
             value=carData['data'],
@@ -330,10 +337,11 @@ app.layout = html.Div(children=[
     ], className='content-section implementation'),
     html.H4('PDF Viewer Preview'),
     ex.PDFViewerComponent(
-            url='https://arxiv.org/pdf/quant-ph/0410100.pdf',
-            navbarOnTop=True
-        )
+        url='https://arxiv.org/pdf/quant-ph/0410100.pdf',
+        navbarOnTop=True
+    )
 ])
+
 
 # @app.callback(Output('output', 'children'), [Input('paginator-default', 'first'),
 #                                                 Input('paginator-default', 'rows')])
@@ -348,6 +356,7 @@ app.layout = html.Div(children=[
 def display_output(text, first, rows):
     # check value
     return 'First value is: {}, Rows value is {}'.format(first, rows)
+
 
 # @app.callback(Output('output0', 'children'), [Input('input3', 'value')])
 # def display_output0(selection):
@@ -364,6 +373,7 @@ def display_output5(selection):
     icon = f"Clicked on: {selection['action'] if selection else 'None'} icon"
     item = f"  of {selection['item'] if selection else 'None'} item"
     return icon + item
+
 
 @app.callback(Output('output1', 'children'), [Input('paginator-default1', 'first'),
                                               Input('paginator-default1', 'rows'), ])
@@ -382,5 +392,26 @@ def tree_output1(selectionKeys):
     return 'Tree selectionKeys: {}'.format(selectionKeys)
 
 
+# 'positionOptions', 'watchPosition', 'userDecisionTimeout', 'suppressLocationOnMount', 'isOptimisticGeolocationEnabled'
+@app.callback(Output('geoloc-out', 'children'),
+              [Input('geoloc-trig', 'n_clicks')],
+              [State('geolocator', 'coords'),
+               State('geolocator', 'positionOptions'),
+               State('geolocator', 'watchPosition'),
+               State('geolocator', 'userDecisionTimeout'),
+               State('geolocator', 'suppressLocationOnMount'),
+               State('geolocator', 'isOptimisticGeolocationEnabled')])
+def button_geoloc(n_clics, coords,posopts,watchpos,usrdec,suppress,isoptim):
+    print(coords)
+    return ''
+
+
+# @app.callback(Output('geolocator-output', 'children'), [Input('load-button', 'n_clicks')], [State('geolocator','geolocationProvider')])
+# def load_button(n_cllicks,coords):
+#     print(coords)
+#     return coords
+# return 'Tree selectionKeys: {}'.format(selectionKeys)
+
 if __name__ == '__main__':
-    app.run_server(host="0.0.0.0", port=int("8080"),debug=True)
+    # app.run_server(debug=True)
+    app.run_server(host="0.0.0.0", port=int("8080"), debug=True)
