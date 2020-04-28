7cb52291f443a006ce07208f4773def5b160d48b
Archer-Sanches <krich7777@gmail.com>
2020-04-28 13:00:13 -0400

    merge recent Trello component and geolocator components

--- R/trelloComponent.R
+++ R/trelloComponent.R
@@ -1,8 +1,8 @@
 # AUTO GENERATED FILE - DO NOT EDIT
 
-trelloComponent <- function(draggable=NULL, laneDraggable=NULL, cardDraggable=NULL, collapsibleLanes=NULL, editable=NULL, canAddLanes=NULL, hideCardDeleteIcon=NULL, editLaneTitle=NULL) {
+trelloComponent <- function(id=NULL, value=NULL, draggable=NULL, laneDraggable=NULL, cardDraggable=NULL, collapsibleLanes=NULL, editable=NULL, canAddLanes=NULL, hideCardDeleteIcon=NULL, editLaneTitle=NULL, changedData=NULL, cardId=NULL, landId=NULL, event=NULL, sourceLaneId=NULL, targetLaneId=NULL, position=NULL, cardDetails=NULL, landParams=NULL, metaData=NULL, card=NULL) {
     
-    props <- list(draggable=draggable, laneDraggable=laneDraggable, cardDraggable=cardDraggable, collapsibleLanes=collapsibleLanes, editable=editable, canAddLanes=canAddLanes, hideCardDeleteIcon=hideCardDeleteIcon, editLaneTitle=editLaneTitle)
+    props <- list(id=id, value=value, draggable=draggable, laneDraggable=laneDraggable, cardDraggable=cardDraggable, collapsibleLanes=collapsibleLanes, editable=editable, canAddLanes=canAddLanes, hideCardDeleteIcon=hideCardDeleteIcon, editLaneTitle=editLaneTitle, changedData=changedData, cardId=cardId, landId=landId, event=event, sourceLaneId=sourceLaneId, targetLaneId=targetLaneId, position=position, cardDetails=cardDetails, landParams=landParams, metaData=metaData, card=card)
     if (length(props) > 0) {
         props <- props[!vapply(props, is.null, logical(1))]
     }
@@ -10,7 +10,7 @@ trelloComponent <- function(draggable=NULL, laneDraggable=NULL, cardDraggable=NU
         props = props,
         type = 'TrelloComponent',
         namespace = 'dash_extra_ui_components',
-        propNames = c('draggable', 'laneDraggable', 'cardDraggable', 'collapsibleLanes', 'editable', 'canAddLanes', 'hideCardDeleteIcon', 'editLaneTitle'),
+        propNames = c('id', 'value', 'draggable', 'laneDraggable', 'cardDraggable', 'collapsibleLanes', 'editable', 'canAddLanes', 'hideCardDeleteIcon', 'editLaneTitle', 'changedData', 'cardId', 'landId', 'event', 'sourceLaneId', 'targetLaneId', 'position', 'cardDetails', 'landParams', 'metaData', 'card'),
         package = 'dashExtraUiComponents'
         )
 
